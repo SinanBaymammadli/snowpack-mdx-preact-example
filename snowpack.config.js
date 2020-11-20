d535994e04359bb3bd9a5cd26684f901d912df86
Sinan Baymammadli <sinan.baymammadli@gmail.com>
2020-11-20 14:23:07 +0400

    initial commit

---
+++ snowpack.config.js
@@ -0,0 +1,30 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/_dist_',
+  },
+  plugins: [
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-typescript',
+    '@prefresh/snowpack',
+  ],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    installTypes: true,
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
