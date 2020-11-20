6b832553b09bdc1d19b404dd73d6a3b518655c12
Sinan Baymammadli <sinan.baymammadli@gmail.com>
2020-11-20 14:49:39 +0400

    setup

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,13 +1,21 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: '/',
-    src: '/_dist_',
+    public: "/",
+    src: "/_dist_",
   },
   plugins: [
-    '@snowpack/plugin-dotenv',
-    '@snowpack/plugin-typescript',
-    '@prefresh/snowpack',
+    "@snowpack/plugin-babel",
+    "@snowpack/plugin-dotenv",
+    "@snowpack/plugin-typescript",
+    "@prefresh/snowpack",
+    [
+      "snowpack-plugin-mdx",
+      {
+        renderer: `import { h } from "preact";
+                   import { mdx } from "@mdx-js/preact"`,
+      },
+    ],
   ],
   install: [
     /* ... */
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
-import { h, render } from 'preact';
-import 'preact/devtools';
-import App from './App.js';
-import './index.css';
+import { h, render } from "preact";
+import "preact/devtools";
+import { MDXProvider } from "@mdx-js/preact";
+import { App } from "./App.js";
+import "./index.css";
 
-const root = document.getElementById('root')
+const root = document.getElementById("root");
 
 if (root) {
-  render(<App />, root);
+  render(
+    <MDXProvider components={{}}>
+      <App />
+    </MDXProvider>,
+    root,
+  );
 }
